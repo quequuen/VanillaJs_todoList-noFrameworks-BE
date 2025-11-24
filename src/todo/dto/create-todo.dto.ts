950d86a2e6e5b8e84f17488a6c9879c00d97ffa9
quequuen <rkdusl2020@gmail.com>
2025-11-24 14:21:36 +0900

    fix: 프론트 요청 구조에 맞게 Todo DTO 수정
    - CreateTodoDto에서 title 필드 제거 (Entity에 없음)
    - CreateTodoDto에서 deadLine을 필수 필드로 변경 (프론트에서 항상 보냄)
    - UpdateTodoDto에서 title 필드 제거
    - 프론트는 deadLine, content만 전송하므로 DTO 구조 통일

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -2,12 +2,8 @@ import { IsString, IsOptional, IsDateString } from 'class-validator';
 
 export class CreateTodoDto {
   @IsString()
-  title: string;
-
-  @IsString()
   content: string;
 
-  @IsOptional()
   @IsDateString()
-  deadLine?: string; // 프론트에서 date string 형식으로 전송
+  deadLine: string;
 }
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -3,15 +3,12 @@ import { IsString, IsOptional, IsBoolean, IsDateString } from 'class-validator';
 export class UpdateTodoDto {
   @IsOptional()
   @IsString()
-  title?: string;
-
-  @IsOptional()
-  @IsString()
   content?: string;
 
   @IsOptional()
   @IsDateString()
   deadLine?: string;
+
   @IsOptional()
   @IsBoolean()
   completed?: boolean;
